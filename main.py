227f0b4a54933f2a9c9bf973c3fe9f3e36590758
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-13 16:06:29 +0100

    ready for first test

--- main.py
+++ main.py
@@ -12,38 +12,25 @@ from matplotlib import cm
 NSIDE = 1
 sigma_rbf = 100000
 N_PROCESS_MAX = 45
-N_sample = 5000
+N_sample = 100
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
-def pipeline(tuple_input):
-    sampler, reference_data = tuple_input
-    sampled = sampler.sample_model()
-    sim_data = sampled["sky_map"]
-    discrepency_L2 = compute_discrepency_L2((reference_data, sim_data))
-    discrepency_inf = compute_discrepency_Inf((reference_data, sim_data))
-    return {"sky_map": sim_data, "cosmo_params": sampled["cosmo_params"], "betas": sampled["betas"],
-            "discrepency_L2":discrepency_L2, "discrepency_Inf": discrepency_inf}
-
 
 def main(NSIDE):
-    #reference_data = np.load("data/reference_values/reference_data_simplified.npy")
-    sampler = Sampler(NSIDE)
-    reference = sampler.sample_data()
-    with open("B3DCMB/data/reference_data", "wb") as f:
-        pickle.dump(reference, f)
+    with open("B3DCMB/data/reference_data", "rb") as f:
+        reference_data = pickle.load(f)
 
-    '''
+    sky_map = reference_data["sky_map"]
+
+    sampler = Sampler(NSIDE)
     time_start = time.time()
     pool = mp.Pool(N_PROCESS_MAX)
-    all_results = pool.map(pipeline, ((sampler, reference_data, ) for _ in range(N_sample)))
+    all_results = pool.map(sampler.sample_model, ((sky_map,) for _ in range(N_sample)))
     time_elapsed = time.time() - time_start
     print(time_elapsed)
-    with open("data/simulations/results_inf", "wb") as f:
-        pickle.dump(all_results, f)
-    '''
 
 
 
