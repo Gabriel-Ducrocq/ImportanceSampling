fc2099d0ff6e2ac95bc7024cb237f8c5bac144c8
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-14 16:00:31 +0100

    trying with 1000 sample size

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from matplotlib import cm
 NSIDE = 1
 sigma_rbf = 100000
 N_PROCESS_MAX = 45
-N_sample = 100
+N_sample = 1000
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
@@ -20,7 +20,6 @@ COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
 
 def main(NSIDE):
-    '''
     with open("B3DCMB/data/reference_data_right_beta_sync", "rb") as f:
         reference_data = pickle.load(f)
 
@@ -36,7 +35,6 @@ def main(NSIDE):
     with open("B3DCMB/data/simulated_sample_right_beta_sync2", "wb") as f:
         pickle.dump(all_results, f)
 
-    '''
     with open("B3DCMB/data/simulated_sample_right_beta_sync2", "rb") as f:
         samples = pickle.load(f)
 
@@ -57,6 +55,7 @@ def main(NSIDE):
 
     ess = (np.sum(w)**2)/np.sum(w**2)
     print(ess)
+
 
 if __name__=='__main__':
     main(NSIDE)
