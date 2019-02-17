5ee5a394c9d59263b61a0d3212f737bdccde62ef
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-17 14:16:31 +0100

    running with 10 000

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ from matplotlib import cm
 NSIDE = 1
 sigma_rbf = 100000
 N_PROCESS_MAX = 45
-N_sample = 1000
+N_sample = 10000
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
 def main(NSIDE):
-    '''
     with open("B3DCMB/data/reference_data_right_beta_sync", "rb") as f:
         reference_data = pickle.load(f)
 
@@ -35,7 +34,6 @@ def main(NSIDE):
     with open("B3DCMB/data/simulated_sample_small", "wb") as f:
         pickle.dump(all_results, f)
 
-    '''
     with open("B3DCMB/data/simulated_sample_small", "rb") as f:
         samples = pickle.load(f)
 
@@ -57,7 +55,7 @@ def main(NSIDE):
 
     ess = (np.sum(w)**2)/np.sum(w**2)
     print(ess)
-
+    '''
     plt.hist(log_weights)
     plt.title("Log weights histogram")
     plt.savefig("B3DCMB/figures/log_weights_histogram.png")
@@ -67,6 +65,7 @@ def main(NSIDE):
     plt.title("Histogram of weights")
     plt.savefig("B3DCMB/figures/weights_histogram.png")
     plt.close()
+    '''
 
 if __name__=='__main__':
     main(NSIDE)
