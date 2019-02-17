de142e90f9f338093242d5d6bd52b755ea2f263e
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-17 12:06:29 +0100

    verifying

--- main.py
+++ main.py
@@ -18,8 +18,14 @@ COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
+def test(n):
+    return np.random.randint(10)
 
 def main(NSIDE):
+    pool = mp.Pool(N_PROCESS_MAX)
+    all_results = pool.map(test, (i for i in range(5)))
+    print(all_results)
+    '''
     with open("B3DCMB/data/reference_data_right_beta_sync", "rb") as f:
         reference_data = pickle.load(f)
 
@@ -56,6 +62,7 @@ def main(NSIDE):
 
     ess = (np.sum(w)**2)/np.sum(w**2)
     print(ess)
+    '''
     '''
     plt.hist(log_weights)
     plt.title("Log weights histogram")
