de85d5cf9c7b5fcd8082e9b1e554a4feae50f8ec
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-25 21:47:06 +0100

    first try

--- main.py
+++ main.py
@@ -12,20 +12,13 @@ from matplotlib import cm
 NSIDE = 1
 sigma_rbf = 100000
 N_PROCESS_MAX = 45
-N_sample = 10000
+N_sample = 10
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
 def main(NSIDE):
-    sampler = Sampler(NSIDE)
-    data = sampler.sample_data()
-
-    with open("B3DCMB/data/reference_data", "wb") as f:
-        pickle.dump(data, f)
-
-    """
     with open("B3DCMB/data/reference_data", "rb") as f:
         reference_data = pickle.load(f)
 
@@ -33,20 +26,23 @@ def main(NSIDE):
 
     sampler = Sampler(NSIDE)
     time_start = time.time()
-    pool = mp.Pool(N_PROCESS_MAX)
-    all_results = pool.map(sampler.sample_model, ((sky_map,i,) for i in range(N_sample)))
+    pool1 = mp.Pool(N_PROCESS_MAX)
+    pool2 = mp.Pool(N_PROCESS_MAX)
+    noise_level = 1000
+    all_sample = pool1.map(sampler.sample_model, ((sky_map,i,) for i in range(N_sample)))
+    all_results = pool2.map(sampler.compute_weight, ((data, noise_level, i,) for i,data in all_sample))
     time_elapsed = time.time() - time_start
     print(time_elapsed)
 
+    """
     with open("B3DCMB/data/simulated_sample", "wb") as f:
         pickle.dump(all_results, f)
 
     with open("B3DCMB/data/simulated_sample", "rb") as f:
         samples = pickle.load(f)
+    """
 
-    log_weights = []
-    for res in samples:
-        log_weights.append(res["log_weight"])
+    log_weights = all_results
 
     log_weights = np.array(log_weights)
     print(log_weights)
@@ -62,7 +58,6 @@ def main(NSIDE):
 
     ess = (np.sum(w)**2)/np.sum(w**2)
     print(ess)
-    """
     '''
     plt.hist(log_weights, bins = 200)
     plt.title("Log weights histogram")
