e916b2f26b4a4481a4efea02ebaf79dc89f96310
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-02-26 10:56:10 +0100

    again

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main(NSIDE):
     print(time_elapsed)
 
     with open("B3DCMB/data/simulated_AS", "wb") as f:
-        pickle.dump({"simuated_points":all_sample, "log_weights":log_weights},f)
+        pickle.dump({"simulated_points":all_sample, "log_weights":log_weights},f)
 
     '''
     with open("B3DCMB/data/reference_data_As", "rb") as f:
@@ -63,7 +63,7 @@ def main(NSIDE):
     print(ess)
     #print(time_elapsed)
 
-    histogram_posterior(w, all_results["simulated_points"]["cosmo_params"], reference_data["cosmo_params"])
+    histogram_posterior(w, all_results["simuated_points"]["cosmo_params"], reference_data["cosmo_params"])
     '''
     plt.hist(log_weights, bins = 200)
     plt.title("Log weights histogram")
