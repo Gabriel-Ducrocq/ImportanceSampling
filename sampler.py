c202320377b39bbf3a7fa0ec5601b39300ae7eaa
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-14 00:00:51 +0100

    1000 times std

--- sampler.py
+++ sampler.py
@@ -15,7 +15,7 @@ import pickle
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 MEAN_AS = 3.047
-SIGMA_AS = 0.014*200
+SIGMA_AS = 0.014*1000
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 LiteBIRD_sensitivities = np.array([36.1, 19.6, 20.2, 11.3, 10.3, 8.4, 7.0, 5.8, 4.7, 7.0, 5.8, 8.0, 9.1, 11.4, 19.6])
