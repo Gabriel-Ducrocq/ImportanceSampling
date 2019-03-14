b0b77392fc5b56b6d61c2fa114fc0d545d423793
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-14 13:54:53 +0100

    again

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ def histogram_posterior(weights, cosmo_sample, reference_cosmo):
 
         print("Length of e:" + str(len(e)))
         prior = np.random.normal(COSMO_PARAMS_MEANS[i], 1000*COSMO_PARAMS_SIGMA[i], 10000)
-        plt.hist(prior, density=True, alpha=0.5, label="Prior", bins = 10)
+        plt.hist(prior, density=True, alpha=0.5, label="Prior", bins = 1000)
         plt.hist(e, density = True, alpha = 0.5, label = "Posterior", weights = weights, bins = 10)
         plt.legend(loc='upper right')
         plt.title('Histogram parameter: '+name)
