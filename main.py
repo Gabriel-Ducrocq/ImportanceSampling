3d8aabe351e0ff5197b0e7ae9b7731ffb0735f9f
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-17 14:52:28 +0100

    pandas

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main(NSIDE):
     print(np.max(data))
     print(np.min(data))
     unique, counts = np.unique(data, return_counts=True)
-    d = pd.DataFrame.from_dict(dict({"value":unique, "count":counts/np.sum(count)}))
+    d = pd.DataFrame.from_dict(dict({"value":unique, "count":counts/np.sum(counts)}))
     d = d.sort_values("count", ascending = False)
     print(d.head(10))
 
