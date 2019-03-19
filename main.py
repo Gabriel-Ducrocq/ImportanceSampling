f0bce66247a26bd5d2b35338dc2c5b655bfe1f93
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-19 11:04:15 +0100

    fixing

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main(NSIDE):
     means = prel["means"]
     sigmas_symm = prel["sigmas_symm"]
     denom = ["denom"]
-    
+
     #ref = sampler.sample_data()
     #with open("B3DCMB/data/reference_data_As_NSIDE_512", "wb") as f:
     #    pickle.dump(ref, f)
