5002d0cc9916419b098129290b09266bd9c77642
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-20 17:23:04 +0100

    checking again

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
 COSMO_PARAMS_SIGMA = [0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]
 
 def main(NSIDE):
-    #sampler = Sampler(NSIDE)
+    sampler = Sampler(NSIDE)
 
-    #start_time = time.time()
-    #ref = sampler.sample_data()
-    #with open("B3DCMB/data/reference_data_As_NSIDE_512", "wb") as f:
-    #    pickle.dump(ref, f)
+    start_time = time.time()
+    ref = sampler.sample_data()
+    with open("B3DCMB/data/reference_data_free_beta_NSIDE_512", "wb") as f:
+        pickle.dump(ref, f)
 
-    #print(time.time() - start_time)
+    print(time.time() - start_time)
 
     '''
     start = time.time()
@@ -93,9 +93,10 @@ def main(NSIDE):
         pickle.dump({"simulated_points":all_sample, "log_weights":log_weights},f)
 
     '''
+    '''
     with open("B3DCMB/data/reference_data_all_NSIDE_512", "rb") as f:
         reference_data = pickle.load(f)
-        
+
     with open("B3DCMB/data/simulated_all_NSIDE_512", "rb") as f:
         data = pickle.load(f)
 
@@ -119,7 +120,7 @@ def main(NSIDE):
 
     histogram_posterior(w, all_sample, reference_data["cosmo_params"])
 
-
+    '''
     '''
     with open("B3DCMB/data/simulated_AS_NSIDE_512_reference", "rb") as f:
         ref = pickle.load(f)
