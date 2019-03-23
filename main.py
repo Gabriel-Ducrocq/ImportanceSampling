0f5e121e558f00d690f928910dee0c055797bfbd
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-23 15:13:33 +0100

    fixing

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ import scipy
 
 NSIDE = 512
 sigma_rbf = 100000
-N_PROCESS_MAX = 40
-N_sample = 2
+N_PROCESS_MAX = 30
+N_sample = 50
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
