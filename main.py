ff261ce712b8f3ee47619cfbe9e39499a78c3fa3
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-03-24 21:45:18 +0100

    trying with more shared values

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def main(NSIDE):
         means1 = manager.list(means[:int(len(means)/2)])
         means2 = manager.list(means[int(len(means)/2):])
         print("Means done !")
-        list_sigmas_symm = split(sigmas_symm, self.Npix*2)
+        list_sigmas_symm = np.split(sigmas_symm, self.Npix*2)
         sigmas_symm = [manager.list(k) for k in list_sigmas_symm]
 
         denom = manager.Value('d', denom)
