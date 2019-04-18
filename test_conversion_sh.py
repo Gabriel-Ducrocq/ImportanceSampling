f88a4521a75ef22072e51b5ca96841ee7107b143
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-04-18 15:08:14 +0200

    benchmarking transformation to spherical

--- test_conversion_sh.py
+++ test_conversion_sh.py
@@ -11,14 +11,15 @@ OUTPUT_CLASS = 'tCl pCl lCl'
 
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"]
 COSMO_PARAMS_MEANS = [0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]
+cosmo = Class()
 start = time.clock()
 cosmo_params = {l[0]: l[1] for l in zip(COSMO_PARAMS_NAMES, COSMO_PARAMS_MEANS)}
 params = {'output': OUTPUT_CLASS,
           'l_max_scalars': L_MAX_SCALARS,
           'lensing': LENSING}
 params.update(cosmo_params)
-self.cosmo.set(params)
-self.cosmo.compute()
+cosmo.set(params)
+cosmo.compute()
 cls = self.cosmo.lensed_cl(L_MAX_SCALARS)
 eb_tb = np.zeros(shape=cls["tt"].shape)
 _, Q, U = hp.synfast((cls['tt'], cls['ee'], cls['bb'], cls['te'], eb_tb, eb_tb), nside=self.NSIDE, new=True)
