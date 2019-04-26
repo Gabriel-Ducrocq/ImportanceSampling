bff4dfda5f99868c7e1aebe546c9fdc1dcca481c
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-04-26 16:04:46 +0200

    printing log var and var

--- launcher.py
+++ launcher.py
@@ -17,7 +17,7 @@ lik_evals = []
 var = []
 points = []
 for i, As in enumerate(np.linspace(start = 0.5, stop = 25, num = 50, endpoint = False)):
-    with open("B3DCMB/flatness_" +str(i), "rb") as f:
+    with open("B3DCMB/flatness_bis" +str(i), "rb") as f:
         d = pickle.load(f)
         log_weights = d["log_weights"]
         log_approx = np.max(log_weights) + np.log(np.mean(np.exp(log_weights - np.max(log_weights))))
