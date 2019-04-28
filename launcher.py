d5792f2e2057aee3a50c4aa40797f9ee864d8e88
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-04-28 22:40:53 +0200

    computing confidence interval

--- launcher.py
+++ launcher.py
@@ -20,7 +20,7 @@ for i, As in enumerate(np.linspace(start = 0.5, stop = 25, num = 50, endpoint = 
     with open("B3DCMB/flatness_" +str(i), "rb") as f:
         d = pickle.load(f)
         log_weights = d["log_weights"]
-        log_approx = np.max(log_weights) + np.log(np.mean(np.exp(log_weights - 2*np.min(log_weights))))
+        log_approx = np.max(log_weights) + np.log(np.mean(np.exp(log_weights - np.max(log_weights))))
         #log_var = 2*np.max(log_weights) + np.log(np.mean((np.exp(log_weights - np.max(log_weights)) - np.exp(log_approx - np.max(log_weights)))**2))
         log_var = np.log(np.mean(np.exp(log_weights - np.max(log_weights))**2) - (np.mean(np.exp(log_weights - np.max(log_weights)))**2)) + 2*np.max(log_weights)
         lik_evals.append(log_approx)
