f306da8af2dbfca0035a470d5690a7b3bc3f7482
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-05-09 09:42:43 +0200

    again

--- launcher.py
+++ launcher.py
@@ -51,7 +51,7 @@ for i, As in enumerate(np.linspace(start = 0.5, stop = 25, num = 50, endpoint = 
 cut = 27
 fig, axes = plt.subplots(2, 2, figsize = (10, 10))
 fig.subplots_adjust(wspace = 0.7, hspace = 0.7)
-fig.suptitle("log likelihood in As for 'true value' of As = 3.049")
+fig.suptitle("log likelihood in As for 'true value' of As = 3.034")
 axes[0,0].plot(points, lik_evals, "blue")
 axes[0, 0].set_title("log likelihood")
 axes[0, 0].set_xlabel("As")
