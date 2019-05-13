cdad0fdd18fea1778cbfcab94ae125b7f5629575
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-05-13 10:53:13 +0200

    adding no noise to reference map

--- main.py
+++ main.py
@@ -74,6 +74,11 @@ def main(NSIDE, run_num, As):
     start_time = time.time()
     sampler = Sampler(NSIDE, As)
 
+    data = sampler.sample_data()
+    with open("B3DCMB/data/reference_data_As_NSIDE_512_no_noise", "wb") as f:
+        pickle.dump(data, f)
+
+    """
     with open("B3DCMB/data/reference_data_As_NSIDE_512", "rb") as f:
         reference_data = pickle.load(f)
 
@@ -120,6 +125,7 @@ def main(NSIDE, run_num, As):
 
     print("Script number " + str(run_num) + " took " + str(time_elapsed) + "seconds")
     #return np.max(log_weights) + np.log(np.mean(np.exp(log_weights - np.max(log_weights))))
+    """
     """
     w = np.exp(log_weights - np.max(log_weights))
     w = w/np.sum(w)
