11a9c8738bc46fc47a671ab51f2565b1fd2552fc
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-05-22 21:41:32 +0200

    checking shape of Cls

--- metropolis_test.py
+++ metropolis_test.py
@@ -37,6 +37,8 @@ def sample_power_spectrum(cosmo_params):
 
 cls_tt = sample_power_spectrum([0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561])
 TRUE_MAP = hp.synalm(cls_tt, new=True)
+print("TRUE MAP shape")
+print(TRUE_MAP.shape)
 
 def compute_posterior(cls, params):
     prior = (-1/2)*np.sum(((params - np.array(COSMO_PARAMS_MEANS))**2)/(np.array(COSMO_PARAMS_SIGMA)**2)) - (1/2)*(np.sum(np.log(np.array(COSMO_PARAMS_SIGMA)**2)))
