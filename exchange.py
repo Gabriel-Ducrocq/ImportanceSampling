b862533fc7dca6cceaa33d65cf7b09627e3c535b
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-05-24 23:46:36 +0200

    calibrating acceptance rate

--- exchange.py
+++ exchange.py
@@ -106,6 +106,6 @@ np.save("B3DCMB/exhange.npy", np.array(path))
 if PLOT:
     path = np.load("B3DCMB/exhange.npy")
     first, second, third, fourth, fifth, sixth = zip(*path)
-    plt.hist(fifth)
+    plt.hist(fifth[2000:])
     plt.savefig("B3DCMB/exchange.png")
 
