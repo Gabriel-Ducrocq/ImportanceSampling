86180a9b183f38d3db6a3fa1c01cd0a5b5fb14a6
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2019-05-24 19:05:35 +0200

    calibrating acceptance rate

--- exchange.py
+++ exchange.py
@@ -14,7 +14,7 @@ COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_
 COSMO_PARAMS_MEANS = np.array([0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561])
 COSMO_PARAMS_SIGMA = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])
 
-proposal_sigma= COSMO_PARAMS_SIGMA/5
+proposal_sigma= COSMO_PARAMS_SIGMA/7
 
 N_iteration = 100
 
